28608a04af9e5237b2465bd13c0f6c56412fcb8c
owpon <owponjames@gmail.com>
2017-12-18 15:06:14 +0800

    my first commit

---
+++ src/main/java/com/ziv/TestProject/TestSeleniumChrome.java
@@ -0,0 +1,26 @@
+package com.ziv.TestProject;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestSeleniumChrome {
+
+	private static WebDriver driver = null;
+
+	public static void main(String[] args) {
+		// 告訴selenium driver的位置
+		System.setProperty("webdriver.chrome.driver", "C:\\seleniumDriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Testing Search ");
+		element.submit();
+		// driver.quit();
+	}
+
+}
